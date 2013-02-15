27230d41f9ffe3cc06f9d3fa4c88af73b2e560b7
Chris Longo <clongo1@insight.rr.com>
2013-02-15 13:43:13 -0500

    Committing the code

---
+++ OneToOneHundred/OneToOneHundred/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OneToOneHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = string.Empty;
+            for (int number = 1; number < 101; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    output = "Modern";
+                }
+                if (number % 5 == 0)
+                {
+                    output += "Medical";
+                }
+                if (string.IsNullOrEmpty(output))
+                {
+                    Console.WriteLine(number);
+                }
+                else
+                {
+                    Console.WriteLine(output);
+                    output = string.Empty;
+                }
+            }
+        }
+    }
+}
